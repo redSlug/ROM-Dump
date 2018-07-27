7135450d74a2b6ceabd2e01e4b52024c221ab03f
Bradley Dettmer <11279144+redSlug@users.noreply.github.com>
2018-07-26 21:00:11 -0400

    removed noise :), adding new dump, images and code updates

--- knit_k2g_together_rom.py
+++ knit_k2g_together_rom.py
@@ -1,29 +1,31 @@
 
-
-
-
-
 def get_data(file_name):
     with open(file_name, 'r') as f:
         bits = f.readlines()[1:]
         return [int(l.split(',')[1], 16) for l in bits]
 
+low_bits_measured = get_data('full_dump_d0_d6_rising_edge_10us.txt')
+high_bits_measured = get_data('full_dump_d0d5_d7_rising_edge.txt')
 
+length = 2**20
+bit0_6 = 0x7F
+bit_6 = 0x40
 
-high_bits = get_data('a19_high_d0_d5_plus_d7.txt')
-low_bits = get_data('a19_high_d0_d6.txt')
-
-length = min(len(low_bits), len(high_bits))
-
-#print(length)
-#diffing_indices = [(i, a, b) for i, (a, b) in enumerate(zip(high_bits[:length], low_bits[:length])) if ((a & 0x3F) != (b & 0x3F)) and (a != 0 and b != 0)]
-
-#print(diffing_indices)
-
-#print(len(diffing_indices))
-
-rom = bytes(((high & 0x40)) << 1 | (low & 0x7F) for high, low in zip(high_bits[:length], low_bits[:length]))
-with open('rom_high.bin', 'wb') as f:
+# shifting bit 6 to bit 7
+rom = bytes(((high & bit_6)) << 1 | (low & bit0_6) for low, high in zip(low_bits_measured[:length], high_bits_measured[:length]))
+with open('rom_full.bin', 'wb') as f:
     f.write(rom)
 
 
+# verify
+with open('rom_official.bin', 'rb') as f:
+    actual_rom = f.read()[:2**19]
+
+first_6_bit_mask = 0x3F
+full_mask = 0xFF
+diff_mask = full_mask
+
+diffing_indices = [(i, a, b) for i, (a, b) in enumerate(zip(rom[:length], actual_rom[:length])) if ((a & diff_mask) != (b & diff_mask))]
+print(length)
+print(len(diffing_indices))
+# 14056 / 531007 = 3% noisy with falling edge
--- sketch_jun07a/sketch_jun07a.ino
+++ sketch_jun07a/sketch_jun07a.ino
@@ -1,7 +1,10 @@
-#include<Arduino.h>
+  #include<Arduino.h>
 
 int output_enabled = 13;
-int delay_time = 100; // 100 * 2 *  (2**19) microseconds to seconds = 104 seconds
+int a_19_snes = 14;
+int delay_time = 10; 
+// total runtime 100 * 2 (delays) *  (2**20) microseconds to seconds = 208 seconds
+// note this estimate becomes inaccurate for shorter delays, as we approach CPU clock period
 
 // serial to parallel
 int stp_clock = 0;
@@ -14,6 +17,8 @@ void setup() {
   pinMode(output_enabled, OUTPUT);
   
   digitalWrite(output_enabled, HIGH);
+
+  pinMode(a_19_snes, OUTPUT);
   
   pinMode(stp_clock, OUTPUT);
   pinMode(stp_data, OUTPUT);
@@ -37,36 +42,33 @@ void write_8_bit_num(int n) {
 void loop() {    
 
   write_8_bit_num(0);
-  delay(500);
+  delay(1000);
   write_8_bit_num(255);
-  delay(500);
+  delay(1000);
   write_8_bit_num(0);
-  delay(500);
+  delay(1000);
   write_8_bit_num(255);
-  delay(500);
+  delay(1000);
 
-  unsigned long highest_address_we_can_afford = 1L << (11 + 8);
+  unsigned long highest_address_we_can_afford = 1L << (11 + 8 + 1);
 
   for (unsigned long i = 0; i<highest_address_we_can_afford; i++) {
     
     // A0 to A10 are coming from the Arduino directly
     for (unsigned long j = 0; j < 11; j++) {
-      if (i & (1 << j)) {
-        digitalWrite(j + 2, HIGH);
-      } else {
-        digitalWrite(j + 2, LOW);
-      }   
-    }  
+        digitalWrite(j + 2, (i & (1L << j)) == 0 ? LOW : HIGH);
+    }
 
-     // writing highest bits, A11 to A18 to MC140158
-     // A19 goes to ground
+     // writing A11 to A18 via MC140158
      unsigned long low_bits_overflow_threshold = 1L << 11;
      if (i % low_bits_overflow_threshold == 0) {
       // only update higher bits when the value gets updated
       write_8_bit_num(i>>11); 
      }
-     
 
+    // writing A19 highest bit
+    digitalWrite(a_19_snes, (i & (1L << 19)) == 0 ? LOW : HIGH);
+     
     delayMicroseconds(delay_time);
     digitalWrite(output_enabled, LOW);
     delayMicroseconds(delay_time);
@@ -80,7 +82,7 @@ void loop() {
 
   // SIDE 1
   // pin1   MC140158 4          A17
-  // pin2   MC140158 5 / Ground A18 (highest bit / unused - not enough pins)
+  // pin2   MC140158 5          A18 
   // pin3   MC140158 10         A15  
   // pin4   MC140158 11         A12
   // pin5   Arduino 9           A7
@@ -99,7 +101,7 @@ void loop() {
   // SIDE 2
   // pin32  5V_ energy          Vcc
   // pin31  Arduino 13          /OE (Output Enabled)
-  // pin30  Ground / MC140158 5 A19 (unused - not enough pins / highest bit)
+  // pin30  Arduino A0        A19 (highest bit)
   // pin29  MC140158 13         A14
   // pin28  MC140158 12         A13
   // pin27  Arduino 10          A8
